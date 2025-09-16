13ed2dd1e80ed019010b891475e3bae81794a1cc
Sukrtia2134 <sugritabursuk@gmail.com>
2025-09-16 12:49:54 +0700

    finished

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -24,8 +24,8 @@ export default function MainLayout() {
       <AppShell.Footer>
         <FooterComponent
           year="2025"
-          fullName="chanadda"
-          studentId="670612039"
+          fullName="sukrita buasook"
+          studentId="670612134"
         />
       </AppShell.Footer>
 
